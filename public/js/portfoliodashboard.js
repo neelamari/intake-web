b5f5bbd6b473fb7f64f8523cb68b164759fc2adf
neelamari <neelamari@icloud.com>
2016-09-12 09:10:00 -0700

    update

--- public/js/portfoliodashboard.js
+++ public/js/portfoliodashboard.js
@@ -27,31 +27,42 @@ function getTickFormat(d){
 //build line
 function buildLine(ds) {
 
-    // console.log('xscale-max: '+ d3.max(ds.monthlyCount, function (d){ return d.month; }));
-    // console.log('yscale-max: '+ d3.max(ds.monthlyCount, function (d){ return d.count; }));
+    // console.log('xscale-max: '+ d3.max(ds.monthlyprojectCount, function (d){ return d.month; }));
+    // console.log('yscale-max: '+ d3.max(ds.monthlyprojectCount, function (d){ return d.projectcount; }));
 
-    var minDate = getDate(ds.monthlyCount[0]['month']);
-    var maxDate = getDate(ds.monthlyCount[ds.monthlyCount.length-1]['month']);
+    var minDate = getDate(ds.monthlyprojectCount[0]['month']);
+    var maxDate = getDate(ds.monthlyprojectCount[ds.monthlyprojectCount.length-1]['month']);
     
     // console.log("min: " +minDate);
     // console.log("max: " +maxDate);
 
+    //tooltip
+    var tooltip = d3.select("body").append("div")   
+                .attr("class", "tooltip")               
+                .style("opacity", 0);
+
+
+
+    
+
     //scales
     var xScale = d3.time.scale()
                 .domain([minDate, maxDate])                
-                .range([padding+5, w-padding]);
+                .range([padding+5, w-padding])
+                .nice();
     
 
     var yScale = d3.scale.linear()
-                .domain([0, d3.max(ds.monthlyCount, function(d){ return d.count;})])
-                .range([h-padding,10]);
+                .domain([0, d3.max(ds.monthlyprojectCount, function(d){ return d.projectcount;})])
+                .range([h-padding,10])
+                .nice();
     
     var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.time.format("%b"));
     var yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4);
     
     var lineFun = d3.svg.line()
         .x(function (d) {return xScale(getDate(d.month)); } )
-        .y(function (d) {return yScale(d.count); })
+        .y(function (d) {return yScale(d.projectcount); })
         .interpolate("linear");
         
 
@@ -61,24 +72,52 @@ function buildLine(ds) {
                 .attr("class", "y-axis")
                 .attr("transform", "translate(" + padding + ", 0)");
                 
-    var xAxis = svg.append("g").call(xAxisGen)
+    var xAxis = svg.append("g").call(xAxisGen)                        
                 .attr("class","x-axis")
                 .attr("transform", "translate(0," + (h-padding) + ")");
+                
 
     var viz = svg.append("path")
             .attr({
-                d: lineFun(ds.monthlyCount),
-                "stroke" : "cyan",
+                d: lineFun(ds.monthlyprojectCount),
+                "stroke" : "#abdda4",
                 "stroke-width": 2,
                 "fill" : "none",
                 "class": "path-"+ds.category
             });
+
+        var dots = svg.selectAll("circle")                            
+                    .data(ds.monthlyprojectCount)
+                    .enter()
+                    .append("circle")
+                    .attr({
+                        cx: function(d) {return xScale(getDate(d.month)); },
+                        cy: function (d) {return yScale(d.projectcount); },
+                        r:  4,
+                        "fill": "#abd9e9",                                
+                        class: "circle-"+ds.category
+                    }) 
+                    .on("mouseover", function(d) {      
+                        tooltip.transition()        
+                            .duration(500)      
+                            .style("opacity", .9);      
+                        tooltip.html("<strong>projectcount " + d.projectcount + "</strong>")  
+                            .style("left", (d3.event.pageX) + "px")     
+                            .style("top", (d3.event.pageY - 28) + "px");    
+                    })                  
+                    .on("mouseout", function(d) {       
+                        tooltip.transition()        
+                            .duration(500)      
+                            .style("opacity", 0);   
+                    });
+    
+
     }
 
 function updateLine(ds) {
 
-    var minDate = getDate(ds.monthlyCount[0]['month']);
-    var maxDate = getDate(ds.monthlyCount[ds.monthlyCount.length-1]['month']);
+    var minDate = getDate(ds.monthlyprojectCount[0]['month']);
+    var maxDate = getDate(ds.monthlyprojectCount[ds.monthlyprojectCount.length-1]['month']);
     
     //scales
     var xScale = d3.time.scale()
@@ -87,57 +126,67 @@ function updateLine(ds) {
     
 
     var yScale = d3.scale.linear()
-                .domain([0, d3.max(ds.monthlyCount, function(d){ return d.count;})])
+                .domain([0, d3.max(ds.monthlyprojectCount, function(d){ return d.projectcount;})])
                 .range([h-padding,10]);
     
     var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom")
                     .tickFormat(d3.time.format("%b"))
-                    .ticks(ds.monthlyCount.length-1); //adjust number of ticks
+                    .ticks(ds.monthlyprojectCount.length-1); //adjust number of ticks
 
     var yAxisGen = d3.svg.axis().scale(yScale).orient("left")
                     .ticks(4);
     
     var lineFun = d3.svg.line()                
         .x(function (d) {return xScale(getDate(d.month)); } )
-        .y(function (d) {return yScale(d.count); })
+        .y(function (d) {return yScale(d.projectcount); })
         .interpolate("linear");
         
 
     var svg = d3.select("body").select("#svg-"+ds.category);
     
-    var yAxis = svg.selectAll("g.y-axis").call(yAxisGen)                        
+    var yAxis = svg.selectAll("g.y-axis").call(yAxisGen);
                 
-    var xAxis = svg.selectAll("g.x-axis").call(xAxisGen)                        
+    var xAxis = svg.selectAll("g.x-axis").call(xAxisGen);                       
 
     var viz = svg.selectAll(".path-"+ds.category) 
             .transition() //add the transition and you're done!
             .duration(500) //set the duration for more control
             .ease("linear") //choose the type of animation linear, elastic, bounce, circle
             .attr({
-                d: lineFun(ds.monthlyCount)                        
+                d: lineFun(ds.monthlyprojectCount)                        
             });
 
-    
+    //fix for moving dots on update
+    var dots = svg.selectAll(".circle-"+ds.category)                                                   
+            .transition() 
+            .duration(500)
+            .ease("linear")
+            .attr({
+                cx: function(d) {return xScale(getDate(d.month)); },
+                cy: function (d) {return yScale(d.projectcount); }                              
+            });                             
+
 }
     
 //show header
 function showHeader(ds) {
     d3.select("body").append("h1")
-        .text(ds.category + " count (2015)");
+        .text(ds.category + " projectcount (2015)");
 }
 
 
 //get data and draw things  
+//d3.json("https://api.github.com/repos/bsullins/d3js-resources/contents/monthlyprojectCountbyCategoryMultiple.json", function(error, data) {
+    
 d3.json("https://api.github.com/repos/neelamari/intake-web/contents/public/MonthlyCountByStatus.json", function(error, data) {
     
-
     if(error) {
         console.log(error);
     } else {
         // console.log(data); //we're golden!
     }
 
-    var decodedData = JSON.parse(window.atob(data.contents));
+    var decodedData = JSON.parse(window.atob(data.content));
 
     // console.log(decodedData.contents);
 
@@ -158,23 +207,22 @@ d3.json("https://api.github.com/repos/neelamari/intake-web/contents/public/Month
         //get selected option
         var sel = d3.select('#date-option').node().value;
 
-        // console.log(ds.monthlyCount.length-sel);
+        // console.log(ds.monthlyprojectCount.length-sel);
         // console.log(sel);
 
         var decodedData = JSON.parse(window.atob(data.content));
 
             decodedData.contents.forEach(function(ds){
 
-            ds.monthlyCount.splice(0,ds.monthlyCount.length-sel);
+            ds.monthlyprojectCount.splice(0,ds.monthlyprojectCount.length-sel);
 
-            // console.log(ds.monthlyCount.length);
-            
             updateLine(ds);
             
-            
         });
+
 
     })
 
 });  
 
+    
